f26aeeab8c46d622929b79f55c4160189691a3f2
Jhair Guzmán <jhairguz.t@gmail.com>
2021-08-01 22:41:36 -0500

    [persistence] feat: add adapter for truck

---
+++ adapters/despachalo-persistence/src/main/java/org/guzman/despachalo/adapter/persistence/TruckPersistenceAdapter.java
@@ -0,0 +1,22 @@
+package org.guzman.despachalo.adapter.persistence;
+
+import lombok.RequiredArgsConstructor;
+import org.guzman.despachalo.commons.hexagonal.PersistenceAdapter;
+import org.guzman.despachalo.core.distribution.application.port.out.GetTruckPort;
+import org.guzman.despachalo.core.distribution.domain.Truck;
+
+import java.util.Optional;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class TruckPersistenceAdapter implements GetTruckPort {
+    private final TruckRepository repository;
+    private final TruckMapper mapper;
+
+    @Override
+    public Optional<Truck> getTruck(Long truckId) {
+        return repository
+                .findById(truckId)
+                .map(mapper::toTruck);
+    }
+}
