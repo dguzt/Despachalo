449b19e2f05c852b019b5d09e46f47cb897a6aa6
Jhair Guzmán <jhairguz.t@gmail.com>
2021-08-04 00:20:14 -0500

    [web] feat: add error handler for business exceptions

---
+++ adapters/despachalo-web/src/main/java/org/guzman/despachalo/adapter/web/distribution/GetTruckController.java
@@ -0,0 +1,30 @@
+package org.guzman.despachalo.adapter.web.distribution;
+
+import lombok.RequiredArgsConstructor;
+import org.guzman.despachalo.commons.errors.NotFoundException;
+import org.guzman.despachalo.commons.hexagonal.WebAdapter;
+import org.guzman.despachalo.core.distribution.application.TruckNotFoundException;
+import org.guzman.despachalo.core.distribution.application.port.in.GetTruckUseCase;
+import org.guzman.despachalo.core.distribution.domain.Truck;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@WebAdapter
+@RestController
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GetTruckController {
+    private final GetTruckUseCase getTruckUseCase;
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(TruckNotFoundException.class)
+    public ResponseEntity<NotFoundException> handleBookNotFound(RuntimeException ex) {
+        return new ResponseEntity<>((NotFoundException) ex, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(path = "/trucks/{truckId}")
+    public Truck getTruck(@PathVariable Long truckId) {
+        return getTruckUseCase.execute(truckId);
+    }
+}
