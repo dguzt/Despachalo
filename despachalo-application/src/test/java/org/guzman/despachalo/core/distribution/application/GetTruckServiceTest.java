6a7391bb58854c661171e508e2ea38472fcd803e
Jhair Guzmán <jhairguz.t@gmail.com>
2021-08-10 23:43:43 -0500

    [core] chore: config unit testing and example

---
+++ despachalo-application/src/test/java/org/guzman/despachalo/core/distribution/application/GetTruckServiceTest.java
@@ -0,0 +1,55 @@
+package org.guzman.despachalo.core.distribution.application;
+
+import org.guzman.despachalo.core.distribution.application.port.out.GetTruckPort;
+import org.guzman.despachalo.core.distribution.domain.Truck;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class GetTruckServiceTest {
+
+    @Mock
+    private GetTruckPort getTruckPort;
+
+    @InjectMocks
+    private GetTruckService getTruckService;
+
+    private Optional<Truck> truckFoundOptional() {
+        var truck = new Truck(1L, "123456");
+        return Optional.of(truck);
+    }
+
+    @Test
+    void whenLookingForATruck_andExists_shouldReturnIt() {
+        var truckId = 1L;
+        when(getTruckPort.getTruck(truckId))
+                .thenReturn(truckFoundOptional());
+
+        var truck = getTruckService.execute(truckId);
+        assertEquals(truckId, truck.getId());
+    }
+
+    private Optional<Truck> truckNotFoundOptional() {
+        return Optional.empty();
+    }
+
+    @Test
+    void whenLookingForATruck_andNoExists_shouldThrowsTruckNotFoundException() {
+        var truckId = 1L;
+        when(getTruckPort.getTruck(any()))
+                .thenReturn(truckNotFoundOptional());
+
+        assertThatExceptionOfType(TruckNotFoundException.class)
+                .isThrownBy(() -> getTruckService.execute(truckId));
+    }
+}
