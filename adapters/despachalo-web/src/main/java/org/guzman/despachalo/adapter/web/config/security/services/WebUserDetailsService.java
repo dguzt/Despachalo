6bb27b77c9d05bc18e17a002ca741374186abb77
daniel <jhairguz.t@gmail.com>
2021-08-29 01:45:39 -0500

    [web] refactor: logging when user not found with email for user details

--- adapters/despachalo-web/src/main/java/org/guzman/despachalo/adapter/web/config/security/services/WebUserDetailsService.java
+++ adapters/despachalo-web/src/main/java/org/guzman/despachalo/adapter/web/config/security/services/WebUserDetailsService.java
@@ -3,6 +3,8 @@ package org.guzman.despachalo.adapter.web.config.security.services;
 import lombok.RequiredArgsConstructor;
 import org.guzman.despachalo.adapter.web.config.security.model.WebUserDetails;
 import org.guzman.despachalo.adapter.web.config.security.model.WebUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,12 +13,16 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class WebUserDetailsService implements UserDetailsService {
+    Logger logger = LoggerFactory.getLogger(WebUserDetailsService.class);
     private final WebUserRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         var user = repository.findByUserEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+                .orElseThrow(() -> {
+                    logger.info("User not found with email: {}", email);
+                    throw new UsernameNotFoundException("User not found with email: " + email);
+                });
 
         return new WebUserDetails(email, user);
     }
