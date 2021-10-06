fcb56c3639dbb8cf69698552a3b1150305c96f18
Jhair Guzmán <jhairguz.t@gmail.com>
2021-10-05 20:21:54 -0500

    [core] fix: add default constructor to consume usecase

--- despachalo-core/src/main/java/org/guzman/despachalo/core/company/application/port/in/DistributionCenterToRegister.java
+++ despachalo-core/src/main/java/org/guzman/despachalo/core/company/application/port/in/DistributionCenterToRegister.java
@@ -2,10 +2,12 @@ package org.guzman.despachalo.core.company.application.port.in;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.guzman.despachalo.core.common.domain.Location;
 
 @Getter @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class DistributionCenterToRegister {
     String name;
