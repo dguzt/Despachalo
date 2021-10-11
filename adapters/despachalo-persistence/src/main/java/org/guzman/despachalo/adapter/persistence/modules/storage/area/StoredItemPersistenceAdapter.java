739e03c791d1a59397f6e4dd9d1daf5768ab6518
Jhair Guzmán <jhairguz.t@gmail.com>
2021-10-11 16:25:55 -0500

    [persistence] fix: count items stored for order

--- adapters/despachalo-persistence/src/main/java/org/guzman/despachalo/adapter/persistence/modules/storage/area/StoredItemPersistenceAdapter.java
+++ adapters/despachalo-persistence/src/main/java/org/guzman/despachalo/adapter/persistence/modules/storage/area/StoredItemPersistenceAdapter.java
@@ -1,15 +1,20 @@
 package org.guzman.despachalo.adapter.persistence.modules.storage.area;
 
 import lombok.RequiredArgsConstructor;
+import org.guzman.despachalo.adapter.persistence.modules.storage.commodity.ItemEntity;
 import org.guzman.despachalo.adapter.persistence.modules.storage.commodity.ItemMapper;
 import org.guzman.despachalo.adapter.persistence.modules.storage.commodity.ItemRepository;
+import org.guzman.despachalo.adapter.persistence.modules.sync.order.OrderLineRepository;
+import org.guzman.despachalo.adapter.persistence.modules.sync.order.OrderRepository;
 import org.guzman.despachalo.commons.hexagonal.PersistenceAdapter;
 import org.guzman.despachalo.core.storage.application.port.out.AreOrderItemsStoredPort;
 import org.guzman.despachalo.core.storage.application.port.out.GetItemsStoredInAreaPort;
 import org.guzman.despachalo.core.storage.application.port.out.StoreOrderItemsInAreaPort;
 import org.guzman.despachalo.core.storage.domain.Item;
+import org.guzman.despachalo.core.sync.domain.OrderState;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -21,6 +26,8 @@ public class StoredItemPersistenceAdapter implements
         AreOrderItemsStoredPort,
         StoreOrderItemsInAreaPort {
 
+    private final OrderLineRepository orderLineRepository;
+    private final OrderRepository orderRepository;
     private final AreaRepository areaRepository;
     private final ItemRepository itemRepository;
     private final StoreItemRepository storeItemRepository;
@@ -66,5 +73,40 @@ public class StoredItemPersistenceAdapter implements
 
         area.setAvailableCapacity(area.getAvailableCapacity() - rows.size());
         areaRepository.save(area);
+
+        updateCountsForOrderLinesGivenStoredItems(items, orderId);
+    }
+
+    private void updateCountsForOrderLinesGivenStoredItems(List<ItemEntity> items, Long orderId) {
+        var order = orderRepository.findById(orderId).orElseThrow(() -> {
+            var message = String.format("Order not found (order: %d)", orderId);
+            return new RuntimeException(message);
+        });
+
+        var lines = orderLineRepository.findAllByOrderId(orderId);
+        var products = new HashMap<Long, Integer>();
+        items.forEach(item -> {
+            var productId = item.getProductDetailId();
+            var count = products.getOrDefault(productId, 0);
+            products.put(productId, count+1);
+        });
+
+        lines.forEach(line -> {
+            var count = products.getOrDefault(line.getProductDetailId(), 0);
+            var stored = line.getStoredAmount();
+            line.setStoredAmount(stored + count);
+        });
+
+        orderLineRepository.saveAll(lines);
+
+        var noReady = lines.stream()
+                .anyMatch(line -> !Objects.equals(
+                        line.getToSendAmount(),
+                        line.getStoredAmount()));
+
+        if (!noReady) {
+            order.setState(OrderState.READY);
+            orderRepository.save(order);
+        }
     }
 }
